b8aaba775829d34455af868a7c8f558fd9a7d84f
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-07-15 15:24:40 +0300

    UPD burger & menu

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
-new fullpage('#fullpage', {
-	menu: 'menu',
-    // anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-    sectionsColor: ['#ff5f45', '#0798ec'],
-    scrollingSpeed: 900,
-    navigation: true,
-    slidesNavigation: true,
-    navigationTooltips: ['Главная', 'Питание']
-});
+'use strict';
 
 const burger = document.querySelector('.navbar__burger');
 const burgerMenu = document.querySelector('.navbar-burger');
+const first = '#ff5f45', second = '#0798ec';
 
 burger.addEventListener('click', () => {
+    takeColor();
     burger.classList.toggle('active');
     burgerMenu.classList.toggle('active');
 })
 
-window.addEventListener('scroll', function() {
-    if (burgerMenu.classList.contains('active')) {
-        burger.classList.remove('active');
-        burgerMenu.classList.remove('active');
+function takeColor() {
+    let counter = document.body.classList.value,
+        num = counter[counter.length - 1];
+
+    switch (num) {
+        case '0':
+            burgerMenu.style.backgroundColor = first;
+            break;
+        case '1':
+            burgerMenu.style.backgroundColor = second;
+            break;
+        default:
+            console.error('Error');
     }
+}
+
+new fullpage('#fullpage', {
+	menu: 'menu',
+    sectionsColor: [first, second],
+    scrollingSpeed: 900,
+    navigation: true,
+    slidesNavigation: true,
 });
