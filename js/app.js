531d50f61e5b7cd9d3d256eacd4c8266e4c8dc0b
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-07-15 15:02:23 +0300

    UPD design

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+new fullpage('#fullpage', {
+	menu: 'menu',
+    // anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    sectionsColor: ['#ff5f45', '#0798ec'],
+    scrollingSpeed: 900,
+    navigation: true,
+    slidesNavigation: true,
+    navigationTooltips: ['Главная', 'Питание']
+});
+
 const burger = document.querySelector('.navbar__burger');
 const burgerMenu = document.querySelector('.navbar-burger');
 
@@ -5,3 +15,10 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     burgerMenu.classList.toggle('active');
 })
+
+window.addEventListener('scroll', function() {
+    if (burgerMenu.classList.contains('active')) {
+        burger.classList.remove('active');
+        burgerMenu.classList.remove('active');
+    }
+});
