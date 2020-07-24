d9ef4d335a10d6696165a1c921b353ecf4652e57
vdzeer <60068242+vdzeer@users.noreply.github.com>
2020-07-24 15:02:43 +0300

    create final cards and search menu

--- js/app.js
+++ js/app.js
@@ -4,17 +4,10 @@ const burger = document.querySelector('.navbar__burger');
 const burgerMenu = document.querySelector('.navbar-burger');
 const calcItems = document.querySelector('.calc-body-items');
 const calcList = document.querySelector('.calc-ul');
+const input = document.querySelector('.calc-input');
 const first = '#ff5f45', second = '#0798ec', third = '#fc6c7c', fouth = '#fec401';
-
-burger.addEventListener('click', () => {
-    takeColor();
-    burger.classList.toggle('actived');
-    burgerMenu.classList.toggle('actived');
-});
-burgerMenu.addEventListener('click', () => {
-    burger.classList.toggle('actived');
-    burgerMenu.classList.toggle('actived');
-})
+const mass = [];
+let searchText = '';
 
 const getData = async function(url) {
     const response = await window.fetch(url);
@@ -23,6 +16,77 @@ const getData = async function(url) {
     }
     return await response.json();
 };
+
+async function getItems() {
+    await getData('./db/fruits.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/raznoe.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/kasha.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/milk.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/fish.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/xleb.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/sha.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/ovoshi.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/drink.json').then(function(data) {
+        mass.push(...data)
+    });
+    await getData('./db/suhof.json').then(function(data) {
+        mass.push(...data)
+    });
+
+    await seacrhFunc(mass);
+}
+
+function seacrhFunc(arr) {
+    input.onkeypress = function (event) {
+        searchText += event.key;
+        input.onkeydown = (event) => {
+            if (event.key === 'Backspace') searchText = searchText.slice(0, -1);
+            if (searchText === '') {
+            calcItems.innerHTML = '';
+                getData('./db/fruits.json').then(function(data) {
+                    data.forEach(createCard);
+                });
+            } else {
+                findEq(searchText);
+            }
+        }
+        if (searchText === '') {
+            calcItems.innerHTML = '';
+            getData('./db/fruits.json').then(function(data) {
+                data.forEach(createCard);
+            });
+        } else {
+            findEq(searchText);
+        }
+    }
+    
+    function findEq(str) {
+        calcItems.innerHTML = '';
+        let len = str.length;
+        for (const key of arr) {
+            let word = key.name.slice(0, len);
+            if (str !== '' && (str === word || str === word.toLowerCase() || str === word.toUpperCase())) {
+                createCard(key);
+            }
+        }
+    }
+}
 
 function takeColor() {
     let counter = document.body.classList.value,
@@ -59,17 +123,23 @@ function createCard(item) {
     calcItems.insertAdjacentHTML('beforeend', card);
 }
 
-new fullpage('#fullpage', {
-    menu: 'menu',
-    anchors: ['Main', 'Food', 'Calc'],
-    sectionsColor: [first, second, third, fouth],
-    recordHistory: false,
-    scrollingSpeed: 900,
-    verticalCentered: false,
-    navigation: true,
-    slidesNavigation: true,
-    normalScrollElements: '#calcMenu, #calcItems'
+function init() {
+    getData('./db/fruits.json').then(function(data) {
+        data.forEach(createCard);
+    });
+    getItems();
+}
+
+burger.addEventListener('click', () => {
+    takeColor();
+    burger.classList.toggle('actived');
+    burgerMenu.classList.toggle('actived');
 });
+
+burgerMenu.addEventListener('click', () => {
+    burger.classList.toggle('actived');
+    burgerMenu.classList.toggle('actived');
+})
 
 calcList.addEventListener('click', (event) => {
     let target = event.target.textContent;
@@ -134,11 +204,16 @@ calcList.addEventListener('click', (event) => {
     }
 });
 
-function init() {
-    getData('./db/suhof.json').then(function(data) {
-        data.forEach(createCard);
-    });
-}
+new fullpage('#fullpage', {
+    menu: 'menu',
+    anchors: ['Main', 'Food', 'Calc'],
+    sectionsColor: [first, second, third, fouth],
+    recordHistory: false,
+    scrollingSpeed: 900,
+    verticalCentered: false,
+    navigation: true,
+    slidesNavigation: true,
+    normalScrollElements: '#calcMenu, #calcItems'
+});
 
 init();
-
